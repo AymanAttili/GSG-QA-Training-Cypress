d9502b10e183d87e7e875b475b0a9986ccfcc485
Ayman Attili <a.l.ateeli@students.ptuk.edu.ps>
2023-09-21 02:45:00 +0300

    All Done :) :100:

--- cypress/e2e/orangeHRM/firstAssignment.cy.ts
+++ cypress/e2e/orangeHRM/firstAssignment.cy.ts
@@ -36,8 +36,8 @@ describe("Assignment 4", () => {
         // adding new employee using API
         cy.get("@empInfo").then((infoData: any) => {
             // finding emplyeeId
-
             mySideBar.getPage('PIM').click();
+            
             myPIMPAge.elements.addBTN().click();
             myAddEmployeePage.elements.employeeId().then(($el) => {
                 infoData.employeeId = $el[0]._value;
@@ -52,6 +52,7 @@ describe("Assignment 4", () => {
                     },
                 }).then((res) => {
                     infoData.empNumber = res.body.data.empNumber;
+                    // updating empInfo to have empNumber
                     cy.writeFile('../../fixtures/employeeInfo.json', JSON.stringify(infoData));
                 })
 
@@ -61,10 +62,10 @@ describe("Assignment 4", () => {
 
     it("add employee informtion and check", () => {
         cy.get("@empInfo").then((infoData: any) => {
-            cy.visit(`https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/${infoData.empNumber}`);
-            cy.wait(7000);
-
             // Checking that the name saved correctly
+            cy.intercept(`https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/${infoData.empNumber}`).as('empDetails')
+            cy.visit(`https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/${infoData.empNumber}`);
+            cy.wait('@empDetails');
             myEmployeeDetails_Personal.elements.employeeFullName().should('have.text', `${infoData.firstName} ${infoData.lastName}`);
 
             // adding Job details from editJob page
@@ -82,37 +83,34 @@ describe("Assignment 4", () => {
                 infoData.employmentStatus = $el[0].innerText;
             });
             myEmployeeDetails_Job.elements.newJobSaveBTN().click();
-            cy.writeFile('../../fixtures/employeeInfo.json', infoData);
-
 
             // adding supervisor from editReportTo page
             myEditEmployeeNavigation.getPage('Report-to').click({ force: true });
             myEmployeeDetails_ReportTo.elements.addSupervisorBTN().click({ force: true });
             myEmployeeDetails_ReportTo.elements.newSupervisorName().type('a');
+
             cy.wait(3000);
             myEmployeeDetails_ReportTo.elements.autocompleteOption().eq(0).click().then(($el) => {
                 infoData.supervisor = $el[0].innerText;
-                console.log(infoData.supervisor.split(' ')[0] + infoData.supervisor.split(' ')[2])
+                cy.writeFile('../../fixtures/employeeInfo.json', infoData);
                 myEmployeeDetails_ReportTo.elements.addNewSupervisorReportingMethod().click();
                 myEmployeeDetails_ReportTo.elements.selectOption().eq(1).click();
                 myEmployeeDetails_ReportTo.elements.saveNewSupervisorBTN().click({ force: true });
 
+                // search for employee by id using UI
                 mySideBar.getPage('PIM').click();
-
                 myEmployeeSearch.searchBy([{ key: "employeeId", value: infoData.employeeId }]);
 
+                // check that values shown in table is correct
                 myTable.create(['Check', 'Id', 'First (& Middle) Name', 'Last Name', 'Job Title', 'Employment Status', 'Sub Unit', 'Supervisor', 'Actions']);
                 myTable.checkValue(1, 'Id', infoData.employeeId);
                 myTable.checkValue(1, 'First (& Middle) Name', `${infoData.firstName} ${infoData.middleName}`);
                 myTable.checkValue(1, 'Last Name', infoData.lastName);
                 myTable.checkValue(1, 'Job Title', infoData.jobTitle);
-                console.log(infoData)
                 myTable.checkValue(1, 'Employment Status', infoData.employmentStatus);
                 myTable.checkValue(1, 'Sub Unit', infoData.subUnit);
                 myTable.checkValue(1, 'Supervisor', infoData.supervisor.split(' ')[0] + ' ' + infoData.supervisor.split(' ')[2]);
-
             });
-
 
         })
     });
